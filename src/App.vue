0a890e9bf79c57ad92be97c37d68a414bdd0d515
unknown <Kpmanjunath-Hub>
2021-05-03 21:20:16 +0530

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+        <home-hero></home-hero>
+<home-details></home-details>
+<home-plans></home-plans>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+import HomeHero from './components/HomeHero.vue';
+import HomeDetails from './components/HomeDetails.vue';
+import HomePlans from './components/HomePlans.vue';
+
+
+export default {
+    name: 'App',
+    components: {
+        AppNavigation,
+        HomeHero,
+        HomeDetails,
+        HomePlans
+    }
+};
+</script>
+
+<style>
+</style>
